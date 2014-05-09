bbd15b0afb50029f771aae60888f82046b184166
Duong Linh <linhdk@designveloper.com>
2014-05-09 17:31:06 +0700

    Create print_menu page

--- assets/linker/js/routes.js
+++ assets/linker/js/routes.js
@@ -1,13 +1,29 @@
 var AppRouter = Backbone.Router.extend({
 	routes: {
-		dish_menu: 'loadDishMenu'
+		dish_menu: 'loadDishMenu',
+		print_menu:'loadPrintMenu',
+		create_dish: 'createDish'
 	},
 	loadDishMenu: function() {
 		dishMenuCollection = new DishMenuCollection;
 		dishMenuView = new DishMenuView({collection: dishMenuCollection});
 		dishMenuCollection.fetch({data:$.param({page:0,number:5})});
 		$("#main").html(dishMenuView.el);
+	},
+	loadPrintMenu:function(){
+		printMenuCollection = new DishMenuCollection;
+		printMenuView = new PrintMenuView({collection:printMenuCollection});
+		printMenuCollection.fetch();
+		$('#main').html(printMenuView.el);
+	},
+	createDish: function(){
+		dishListCollection = new DishListCollection;
+		dishListView = new DisListView({collection:dishListCollection});
+		dishListCollection.fetch();
+		#('#main').html(dishListView.el);
 	}
+
+
 })
 
 $(function() {
