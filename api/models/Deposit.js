8513c5f0e8ea5fe897e8a17ebc9727d8e1e884dc
Duong Linh <linhdk@designveloper.com>
2014-05-13 17:41:12 +0700

    Create controlloer and model deposit in sails

---
+++ api/models/Deposit.js
@@ -0,0 +1,33 @@
+/**
+ * Deposit
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+    no: {
+      type: 'string'
+    }
+  	date:{
+  		type:'date',
+  		required:true
+  	},
+  	username:{
+  		type:'string',
+  		unique: true
+  	},
+  	amount: {
+  		type: 'integer',
+  		required: true
+  	}
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};
