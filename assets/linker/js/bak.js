948251f4aeee1f3c5a347c04bf559386f79a5a4e
Son Le <thaisonle94@gmail.com>
2014-05-14 14:18:01 +0700

    Merge files

--- assets/linker/js/bak.js
+++ assets/linker/js/bak.js
@@ -1,13 +1,4 @@
 function init(){
-    $('#btn-add').click(function(){
-        add();
-        $('.datepicker').datepicker({      
-        showOn: "button",
-        buttonImage: "images/calendar.png",
-        buttonImageOnly: true,
-    });
-    });
-
     $('.datepicker').datepicker({      
         showOn: "button",
         buttonImage: "images/calendar.png",
@@ -28,39 +19,7 @@ function init(){
         return i++;
     }
     })();
-function add(){
-    //
-    //
-    var tr = document.createElement("tr");
-    var th1 = document.createElement("th");
-    var node=document.createTextNode(incr());
-    th1.appendChild(node);
-    tr.appendChild(th1);
-    //
-    var input = document.createElement("input");
-    input.setAttribute("class","datepicker");
-    input.setAttribute("type","text");
-    var div = document.createElement("div");
-    div.appendChild(input);     
-    var th2 = document.createElement("th");
-    th2.setAttribute("class","mn-cr-edit");
-    th2.appendChild(div);
-    tr.appendChild(th2);
-    for(var i=1;i<8;i++){
 
-        var input = document.createElement("input");
-        input.setAttribute("type","text");
-        input.setAttribute("placeholder","Nhap Mon");
-        var div = document.createElement("div");
-        div.appendChild(input);     
-        var th2 = document.createElement("th");
-        th2.setAttribute("class","mn-cr-edit");
-        th2.appendChild(div);
-        tr.appendChild(th2);
-    }
-    var tbody = document.getElementById("create-menu");
-    tbody.appendChild(tr);
-}
 
 function cleadForm () {
     var f=$('.find > .find-from > div').children('.datepicker').val("");
@@ -71,8 +30,6 @@ function show_popup(name_popup) {
     $('[class*="style_popup"]').hide();
     $(name_popup).show();
     $('.popup_area').show();
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 86770a2544c7c721bb75f35a812f810919740e8d
+
+
