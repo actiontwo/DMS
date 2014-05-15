a97d05dedee7c6b6eeaf9435bceeaa1de5e2f039
Duong Linh <linhdk@designveloper.com>
2014-05-15 18:25:57 +0700

    Restyle Create_Menu page

--- assets/linker/js/views/dishMenu.js
+++ assets/linker/js/views/dishMenu.js
@@ -8,7 +8,7 @@ var DishMenuView = Backbone.View.extend({
         this.render();
     },
     render: function() {
-        this.$el.html(Templates['menu/view_menu']({'dish_menu':this.collection.toJSON()}));
+        this.$el.html(Templates['menu/view_menu']({'dish_menu':this.collection.toJSON(),'list':dishListCollection.toJSON()}));
         for (i in this.collection.models) {
             var model = this.collection.models[i];
             var submenu_view = new SubMenuView({model:model,el:this.$('tr[data-id="' + model.id + '"]')});
