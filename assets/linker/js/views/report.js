75ce31e18d16b4558948db887ff34d186129eaa3
Hien Lam <hienlt@designveloper.com>
2014-05-30 18:14:53 +0700

    get total the cost for each field

--- assets/linker/js/views/report.js
+++ assets/linker/js/views/report.js
@@ -28,13 +28,68 @@ var ReportView = Backbone.View.extend({
 	tagName: 'div',
 	id: 'report',
 	className: 'menus',
+
+// -------------------------------------------------------------------
+// initialize function:
+//
+// PARAMETERS:
+//            
+// RETURNS:
+//           
+// DEPENDENCIES:
+//            
+// PURPOSE:
+//            perform render when collection sysn reset
+// NOTES:
+//            none
+// REVISIONS:
+//            05/30/2014: HienLam
+// -------------------------------------------------------------------
 	initialize: function(){
 		this.listenTo(this.collection, 'sync reset', this.render);
 	},
+
+// -------------------------------------------------------------------
+// render function:
+//
+// PARAMETERS:
+//            
+// RETURNS:
+//           
+// DEPENDENCIES:
+//            
+// PURPOSE:
+//            render to template reportCostMeal.html
+// NOTES:
+//            none
+// REVISIONS:
+//            05/30/2014: Hien Lam
+// -------------------------------------------------------------------
 	render: function(){
 		this.$el.html(Templates['report/reportCostMeal'] ({
 			'report': this.collection.toJSON(),'user':userCollection.toJSON()
 		}));
-
+		var i=1;
+		 $('tbody tr').find('td:first-child').each(function(){
+		 	$(this).html(i);
+		 	i++;
+		 });
+		 
+		 var i;
+		 var array = [];
+		 for(i = 5; i <= 12; i++){
+		 	var sum = 0;
+		 	$('tbody tr').find('td:nth-child(' + i +')').each(function(){
+			 		console.log($(this).text());
+			 		sum += parseFloat($(this).text());
+			  });
+			  array.push(sum);
+			};
+			
+			for(i = 2; i <= 9; i++){
+				$('tbody tr:last-child').find('th:nth-child(' + i + ')').each(function(){
+					$(this).html(array[i-2]);
+				});
+			}
 	}
-})
+});
