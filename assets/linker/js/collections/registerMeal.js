5c79d1db74188aaf7aeac02ea7d3a41d2868cbfe
Son Le <thaisonle94@gmail.com>
2014-05-30 17:37:28 +0700

    Modified all codes using DSV coding style

--- assets/linker/js/collections/registerMeal.js
+++ assets/linker/js/collections/registerMeal.js
@@ -1,4 +1,28 @@
+// =============================================================================
+// ,,,,,,,,, ,,,
+// ,,,,,,,, ,,,  Copyright:
+// ,,,     ,,,          This source is subject to the Designveloper JSC
+// ,,,    ,,,           All using or modify must have permission from us.
+// ,,,   ,,,            http://designveloper.com
+// ,,,,,,,,
+// ,,,,,,,       Name:  DSVScriptTemplate
+//
+// Purpose:
+//          Declare RegisterMealCollection
+// Class:
+//          RegisterMealCollection
+// Called From:
+//          (script) assets/linker/js/routes.js, 
+//			assets/linker/js/views/registerMeal.js,
+//			assets/linker/js/views/viewMealRegistration.js
+// Author:
+//          Son Le (sontl@designveloper.com)
+// Notes:
+//          Additional information [long version]
+// Changelog:
+//          05/30/2014 - Son Le - Added DSV code style.
+// =============================================================================
 RegisterMealCollection = Backbone.Collection.extend({
-	url: "/registermeal",
-	model: RegisterMealModel
+  url: "/registermeal",
+  model: RegisterMealModel
 });
