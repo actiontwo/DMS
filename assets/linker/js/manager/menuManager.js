aa8365bbf277c9fe1f31fd557cde66bd094e7223
Hien Lam <hienlt@designveloper.com>
2014-06-20 17:41:08 +0700

    modify add, delete, edit menu

--- assets/linker/js/manager/menuManager.js
+++ assets/linker/js/manager/menuManager.js
@@ -7,7 +7,7 @@ var MenuManagerCollection = Backbone.Collection.extend({
   url: "/menu",
   model: MenuManagerModel
 });
-var source = {dish: []};
+var dish;
 var MenuManagerView = Backbone.View.extend({
   model: new MenuManagerModel,
   collection: new MenuManagerCollection,
@@ -16,26 +16,26 @@ var MenuManagerView = Backbone.View.extend({
   id: 'dish_menu',
   initialize: function(){
     this.listenTo(this.collection, 'reset change add remove', this.render);
-    $.get('/dish', function(data){      
-        var dish = [];
+    $.get('/dish', function(data){
+        dish = [];
         for(var i = 0; i<data.length; i++){
           dish.push(data[i].dish)
-        }
-        source.dish = dish;
+        }        
     });
   },
   render: function(){
+    console.log(dish);
     this.$el.html(Templates['admin/Manager/menuManager'](this.collection));
-    initDatePicker($('.datepicker'));
-    autoComplete($('.txt-menu'), source.dish);
+    autoComplete($('.txt-dish-create'), dish);
+    initDatePicker($('.datepicker'));    
     this.delegateEvents({
       'click .saveDish': 'saveDish',
       'click .edit-menu': 'editMenu',
       'click .save-menu': 'saveMenu',
       'click .add-menu': 'addMenu',
-      'click .delete-menu': 'deleteMenu'
+      'click .delete-menu': 'deleteMenu',
     })
-    return this;
+    return this;  
   },
   saveDish: function(){
     var dish = $('.dishname').val();
@@ -55,26 +55,60 @@ var MenuManagerView = Backbone.View.extend({
         console.log(err);
       }
     });
-    this.model.urlRoot = '/menuAd';
+    this.model.urlRoot = '/menu';
     $('.dishname').val('');
     $('.dishnote').val('')
   },
   editMenu: function(el){
     var ev = $(el.currentTarget);
-    this.collection.get(ev.parents('tr').data('id')).set({edit: true, update: true})
+    var id = ev.parents('tr').data('id');
+    console.log(id);
+    this.collection.get(ev.parents('tr').data('id')).set({edit: true, update: true });
+
+    // var dishEdit = [];
+    //   $('.dishEdit').find('input.txt-dish-create').each(function(){
+    //     dishEdit.push($(this).val());
+    //   })
+    // var data = {
+    //   edit: true,
+    //   update: true,
+    //   date: $('.input-date-create').val(),
+    //   brunch: $('.select-brunch').val(),
+    //   dish: dishEdit,
+    //   note: $('.txt-note-create').val()
+    // }
+    // console.log(data);
   },
   saveMenu: function(el){
+    // var ev = $(el.currentTarget);
+    // var dishs = [];
+    // ev.parents('tr').find('.dish-loop').each(function(){
+    //   dishs.push($(this).val());
+    // })
+    // var data = {
+    //   edit: false,
+    //   date: ev.parents('tr').find('.txt-date').val(),
+    //   brunch: ev.parents('tr').find('.sel-brunch').val(),
+    //   dish:dishs,
+    //   note: ev.parents('tr').find('.txt-note').val()
+    // }
+    // console.log(data);
+    // var model = this.collection.get(ev.parents('tr').data('id')).set(data);
+    // if(model.attributes.new){
+    //   delete model.attributes.id;
+    // }
+    // model.save();
+
     var ev = $(el.currentTarget);
-    var dishs = [];
-    ev.parents('tr').find('.dish-loop').each(function(){
-      dishs.push($(this).val());
-    })
+    var dishEdit = [];
+      $('.dishEdit').find('input.txt-dish-create').each(function(){
+        dishEdit.push($(this).val());
+      })
     var data = {
-      edit: false,
-      date: ev.parents('tr').find('.txt-date').val(),
-      brunch: ev.parents('tr').find('.sel-brunch').val(),
-      dish:dishs,
-      note: ev.parents('tr').find('.txt-note').val()
+      date: $('.input-date-create').val(),
+      brunch: $('.select-brunch').val(),
+      dish: dishEdit,
+      note: $('.txt-note-create').val()
     }
     console.log(data);
     var model = this.collection.get(ev.parents('tr').data('id')).set(data);
@@ -83,21 +117,20 @@ var MenuManagerView = Backbone.View.extend({
     }
     model.save();
   },
-  addMenu: function(){
-    var whiteSpace = [];
-    for(var i=0; i<5; i++){
-      whiteSpace.push('')
-    }      
+  addMenu: function(){ 
+    var dishGroup = [];
+    $('.txt-dish-create').each(function(){
+      dishGroup.push($(this).val());
+    });
     var data = {
-      id: Math.floor(Math.random() * 10000000000 + 1),
-      new: true,      
-      edit: true,
-      date: '',
-      dish: whiteSpace,
-      note: ''
+      date: $('.input-date-create').val(),
+      brunch: $('.select-brunch').val(),
+      dish: dishGroup,
+      note: $('.txt-note-create').val(),
     };
     console.log(data);
-    this.collection.add(data);
+     var model =  this.collection.add(data);
+     model.save();
   },
   deleteMenu: function(el){
     var ev = $(el.currentTarget);
