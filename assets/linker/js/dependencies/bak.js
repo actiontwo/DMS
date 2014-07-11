906b209437e0cc60d8d30e8e2d5a49c45f3eb700
Hoc Nguyen Viet <hocnv@designveloper.com>
2014-07-11 14:58:56 +0700

    fix DMS-92: if user input wrong date format, it will be alert || add onBlur=validateDate(this) with input have datepicker

--- assets/linker/js/dependencies/bak.js
+++ assets/linker/js/dependencies/bak.js
@@ -48,4 +48,36 @@ Handlebars.registerHelper("math", function (lvalue, operator, rvalue, options) {
   }[operator];
 });
 
-
+function validateDate(el){
+  var date = $(el).val();
+  var dateArray = date.match(/([0-9])/g);
+  var checkflg =true;
+  if (dateArray.length >= 8) {
+    var monthNum = parseInt(dateArray[0] + dateArray[1]);
+    var dateNum = parseInt(dateArray[2] + dateArray[3]);
+    var yearNum = parseInt(dateArray[4] + dateArray[5] + dateArray[6] + dateArray[7]);
+    if ((monthNum == 0) || (monthNum > 12))
+      checkflg = false;
+    else {
+      if (dateNum == 0) checkflg = false;
+      if (monthNum == 2) {
+        if ((yearNum % 4) == 0) {
+          if (dateNum > 29) checkflg = false;
+        } else if (dateNum > 28) checkflg = false;
+      }
+      var month30 = [1, 3, 5, 7, 8, 10, 12];
+      if (month30.indexOf(monthNum) == -1) {
+        if (dateNum > 30) checkflg = false;
+      } else if (dateNum > 31) checkflg = false;
+    }
+  } else checkflg =false;
+  if (!checkflg){
+    alert('wrong date format');
+    $(el).val('');
+    return;
+  }
+  var month = dateArray[0]+dateArray[1];
+  var date  = dateArray[2]+dateArray[3];
+  var year  = dateArray[4]+dateArray[5]+dateArray[6]+dateArray[7];
+  $(el).val(month+'/'+date+'/'+year);
+}
